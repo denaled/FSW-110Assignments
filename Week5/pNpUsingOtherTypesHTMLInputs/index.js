5838f67feb63795aec7c034fe40a329a279d1b8f
denaled <denaled@gmail.com>
2020-01-17 06:38:48 -0600

    FSW110 week5 pNp Other Html inputs commit 1

---
+++ Week5/pNpUsingOtherTypesHTMLInputs/index.js
@@ -0,0 +1,23 @@
+/******************FDW110 Week 5 pNp Using other types of HTML Inputs************************* */
+
+const form = document.Form1
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+   // document.getElementById("test").textContent = form.gender.value 
+   var newLine = "\r\n"
+   const checkedFoods = []
+   for(let i=0; i < form.foodRestrictions.length; i++){
+       if(form.foodRestrictions[i].checked){
+           checkedFoods.push(form.foodRestrictions[i].value)
+       }
+   }
+   
+    confirm("First name: " + form.fName.value + 
+    newLine +"Last Name: " + form.lName.value +
+    newLine +"Age: " + form.age.value +
+    newLine + "Gender: " + form.gender.value +
+    newLine + "Location: " + form.city.value +
+    newLine + "Dietary restrictions: " + checkedFoods)
+    
+})
